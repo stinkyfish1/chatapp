4165a5f35069a443c4e42c853ed01aac85d9833c
stinkyfish1 <156199864+stinkyfish1@users.noreply.github.com>
2025-04-08 10:56:03 +0200

    added: files, still some issues

---
+++ client.py
@@ -0,0 +1,45 @@
+import socket
+from threading import Thread
+
+
+class Client:
+
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((HOST, PORT))
+        self.name = input("Enter your name: ")
+
+        self.talk_to_server()
+
+    def talk_to_server(self):
+        self.socket.send(self.name.encode())
+        Thread(target=self.receive_message).start()
+        self.send_message()
+
+    def send_message(self):
+        while True:
+            client_input = input("")
+            client_message = f"{self.name}: {client_input}"
+            self.socket.send(client_message.encode())
+            if client_input.lower() == "bye":
+                print("You left the chat.")
+                self.socket.close()
+                break
+
+    def receive_message(self):
+        while True:
+            try:
+                server_message = self.socket.recv(1024).decode()
+                if not server_message:
+                    print("Server disconnected.")
+                    self.socket.close()
+                    break
+                print("\033[1;31;40m" + server_message + "\033[0m")
+            except:
+                break
+
+
+if __name__ == "__main__":
+    HOST = '10.58.176.250' 
+    PORT = 1301
+    Client(HOST, PORT)
---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+from threading import Thread
+
+
+class Server:
+    Clients = []
+
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('0.0.0.0', PORT))
+        self.socket.listen(5)
+        print('Server waiting for connections....')
+
+    def listen(self):
+        while True:
+            client_socket, address = self.socket.accept()
+            print("Connection from:", address)
+
+            client_name = client_socket.recv(1024).decode()
+            client = {'client_name': client_name, 'client_socket': client_socket}
+
+            self.broadcast_message(client_name, f"{client_name} has joined the chat")
+
+            Server.Clients.append(client)
+            Thread(target=self.handle_new_client, args=(client,)).start()
+
+    def handle_new_client(self, client):
+        client_name = client['client_name']
+        client_socket = client['client_socket']
+        while True:
+            try:
+                client_message = client_socket.recv(1024).decode()
+                if not client_message or client_message.strip() == f"{client_name}: bye":
+                    self.broadcast_message(client_name, f"{client_name} has left the chat!")
+                    Server.Clients.remove(client)
+                    client_socket.close()
+                    break
+                else:
+                    self.broadcast_message(client_name, client_message)
+            except:
+                Server.Clients.remove(client)
+                client_socket.close()
+                break
+
+    def broadcast_message(self, sender_name, message):
+        for client in Server.Clients:
+            try:
+                client['client_socket'].sendall(f"{sender_name}: {message}".encode())
+            except:
+                client['client_socket'].close()
+                Server.Clients.remove(client)
+
+
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    PORT = 1234
+    server = Server(HOST, PORT)
+    server.listen()
